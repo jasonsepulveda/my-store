f01b65fc1945f81126d73163dd40f78b7b06396e
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-09-23 19:22:05 -0500

    class 4 - routing with Express

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello my server in express')
 });
 
+app.get('/new-root', (req, res) => {
+  res.send('Hello, I am a new root')
+});
+
+// Routing with Express.JS
+// I added a new endpoint in order to send a json object
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  })
+});
+
 app.listen(port, () => {
   console.log('My Port: ' + port)
 });
