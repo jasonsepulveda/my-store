3d772f7ff053e45a5b50cc232540ddb5495eac7a
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-09-24 14:46:52 -0500

    class 19 - error handle with Boom

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom')
 
 class ProductsService {
   constructor() {
@@ -17,6 +18,7 @@ class ProductsService {
         category: faker.commerce.department(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.url(),
+        isBlock: faker.datatype.boolean()
       });
     }
   }
@@ -35,13 +37,20 @@ class ProductsService {
   }
 
   async findOne(id) {
-    return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if(!product) {
+      throw boom.notFound('Product not found')
+    }
+    if(product.isBlock) {
+      throw boom.conflict('Product is blocked')
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1) {
-      throw new Error('it could be updated, product not found')
+      throw boom.notFound('it could not be updated, product not found')
     }
     const product = this.products[index];
     this.products[index] = {
@@ -54,7 +63,7 @@ class ProductsService {
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1) {
-      throw new Error('it could be deleted, product not found')
+      throw boom.notFound('it could not be deleted, product not found')
     }
     this.products.splice(index, 1);
     return {
