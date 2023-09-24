907b9925d8a9f18beee8329ee57de7fc2b963c0d
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-09-23 19:57:08 -0500

    class 7 - new endpoint with 2 parameters

--- index.js
+++ index.js
@@ -13,11 +13,37 @@ app.get('/new-root', (req, res) => {
 // Routing with Express.JS
 // I added a new endpoint in order to send a json object
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 1000,
-  })
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    }
+  ])
 });
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  })
+})
+// new endpoint with 2 parameters
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId } = req.params;
+  const { productId } = req.params
+  res.json({
+    categoryId,
+    productId,
+    name: 'Product 2',
+    price: 2000,
+  })
+})
 
 app.listen(port, () => {
   console.log('My Port: ' + port)
